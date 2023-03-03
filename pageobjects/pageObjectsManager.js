47db6f8ad6eaaf23af02b91841ba1e655f24b60d
NataliaFiryn <natalia.firyn@itmagination.com>
2023-03-03 15:49:36 +0100

    Dropdowns Checkboxes and Radiobuttons added

--- pageobjects/pageObjectsManager.js
+++ pageobjects/pageObjectsManager.js
@@ -1,22 +1,27 @@
-const {ContactUsPage} = require ('./contactUsPage');
+const { ContactUsPage } = require('./contactUsPage');
 const { DatapickerPage } = require('./datapickerPage');
-const { AutocompleteTextFieldPage} = require('./autocompleteTextFieldPage')
+const { AutocompleteTextFieldPage } = require('./autocompleteTextFieldPage')
+const { DropdownsCheckboxesRadioButtonsPage } = require('./dropdownsCheckboxesRadioButtons')
 
 class PageObjectsManager {
-    constructor (page) { 
+    constructor(page) {
         this.page = page
         this.contactUsPage = new ContactUsPage(this.page)
         this.datapickerPage = new DatapickerPage(this.page)
         this.autocompleteTextFieldPage = new AutocompleteTextFieldPage(this.page)
+        this.dropdownsCheckboxesRadioButtonsPage = new DropdownsCheckboxesRadioButtonsPage(this.page)
     }
-    getContactUsPage(){
+    getContactUsPage() {
         return this.contactUsPage
     }
-    getDatapickerPage(){
+    getDatapickerPage() {
         return this.datapickerPage
     }
-    getAutocompleteTextFieldPage(){
+    getAutocompleteTextFieldPage() {
         return this.autocompleteTextFieldPage
     }
+    getDropdownsCheckboxesRadioButtonsPage() {
+        return this.dropdownsCheckboxesRadioButtonsPage
+    }
 }
-module.exports = {PageObjectsManager};
+module.exports = { PageObjectsManager };
--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -84,6 +84,86 @@ test.describe('Datapicker', () => {
         await datapickerPage.InsertDate('2033', 'February', '15')
     });
 });
+test.describe('Dropdown Menu(s), Checkboxe(s) & Radio Button(s)', () => {
+    test.beforeEach(async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.goToDropdownsCheckboxesRadioButtonsPage()
+    });
+    //Dropdown Menu(s)
+    test('First dropdown', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFirstDropdownList('JAVA')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFirstDropdownList('C#')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFirstDropdownList('Python')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFirstDropdownList('SQL')
+        await dropdownsCheckboxesRadioButtonsPage.optionsCounterFirstDropdown(4)
+    });
+    test('Second dropdown', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionSecondDropdownList('Eclipse')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionSecondDropdownList('Maven')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionSecondDropdownList('TestNG')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionSecondDropdownList('JUnit')
+        await dropdownsCheckboxesRadioButtonsPage.optionsCounterSecondDropdown(4)
+    });
+    test('Third dropdown', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionThirdDropdownList('HTML')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionThirdDropdownList('CSS')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionThirdDropdownList('JavaScript')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionThirdDropdownList('JQuery')
+        await dropdownsCheckboxesRadioButtonsPage.optionsCounterThirdDropdown(4)
+    });
+    //Checkboxe(s)
+    test('Chceck and uncheck checkboxes', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.checkAndUncheck('Option 1')
+        await dropdownsCheckboxesRadioButtonsPage.checkAndUncheck('Option 2')
+        await dropdownsCheckboxesRadioButtonsPage.checkAndUncheck('Option 3')
+        await dropdownsCheckboxesRadioButtonsPage.checkAndUncheck('Option 4')
+        await dropdownsCheckboxesRadioButtonsPage.checkNuberOfCheckboxes(4)
+    });
+    test('Check all checkboxe(s) and uncheck selected checkboxes', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.checkAllCheckboxes()
+        await dropdownsCheckboxesRadioButtonsPage.uncheckSelectedCheckboxes(['Option 2', 'Option 4'])
+    });
+    //Radio Button(s)
+    test('Click and check every radio button', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.clickRadioButton('green')
+        await dropdownsCheckboxesRadioButtonsPage.clickRadioButton('blue')
+        await dropdownsCheckboxesRadioButtonsPage.clickRadioButton('yellow')
+        await dropdownsCheckboxesRadioButtonsPage.clickRadioButton('orange')
+        await dropdownsCheckboxesRadioButtonsPage.clickRadioButton('purple')
+        await dropdownsCheckboxesRadioButtonsPage.checkNuberOfRadioButtons(5)
+    });
+    //Selected & Disabled
+    test('Fruits dropdown', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFruitDropdownList('Apple')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFruitDropdownList('Orange')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFruitDropdownList('Pear')
+        await dropdownsCheckboxesRadioButtonsPage.selectOptionFruitDropdownList('Grape')
+        await dropdownsCheckboxesRadioButtonsPage.optionsCounterFruitDropdown(4)
+    });
+    test('Vegetable Radio Buttons', async ({ page }) => {
+        const pageObjectsManager = new PageObjectsManager(page)
+        const dropdownsCheckboxesRadioButtonsPage = pageObjectsManager.getDropdownsCheckboxesRadioButtonsPage()
+        await dropdownsCheckboxesRadioButtonsPage.clickVegetableRadiobuttons('pumpkin')
+        await dropdownsCheckboxesRadioButtonsPage.clickVegetableRadiobuttons('lettuce')
+        await dropdownsCheckboxesRadioButtonsPage.clickVegetableRadiobuttons('cabbage')
+        await dropdownsCheckboxesRadioButtonsPage.optionsCounterVegetableButtons(3)
+    });
+});
 test.describe('Autocomplete Text Field', () => {
     test.beforeEach(async ({ page }) => {
         const pageObjectsManager = new PageObjectsManager(page)
