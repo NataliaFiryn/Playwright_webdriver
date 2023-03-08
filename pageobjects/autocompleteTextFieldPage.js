1421b600d7d80677c0026cf8310193e44beec88a
NataliaFiryn <natalia.firyn@itmagination.com>
2023-03-08 15:15:30 +0100

    Typo corrected

--- pageobjects/autocompleteTextFieldPage.js
+++ pageobjects/autocompleteTextFieldPage.js
@@ -2,7 +2,7 @@ const { expect } = require('@playwright/test');
 class AutocompleteTextFieldPage {
     constructor(page) {
         this.page = page;
-        this.AutocompleteTextFieldLink = page.locator('a#autocomplete-textfield')
+        this.autocompleteTextFieldLink = page.locator('a#autocomplete-textfield')
         this.textField = page.locator('#myInput[type="text"]')
         this.autocompleteListElements = page.locator('#myInputautocomplete-list strong')
         this.submitButton = page.locator('#submit-button')
@@ -12,19 +12,19 @@ class AutocompleteTextFieldPage {
         await this.AutocompleteTextFieldLink.evaluate((el) => el.removeAttribute('target'))
         await this.AutocompleteTextFieldLink.click()
     }
-    async typeText(inputText){
-    await this.textField.type(inputText)
-    const listElementsCounter = await this.autocompleteListElements.count()
-    for(let i = 0; i <listElementsCounter; ++i){
-        await expect(this.autocompleteListElements.nth(i)).toHaveText(inputText)
+    async typeText(inputText) {
+        await this.textField.type(inputText)
+        const listElementsCounter = await this.autocompleteListElements.count()
+        for (let i = 0; i < listElementsCounter; ++i) {
+            await expect(this.autocompleteListElements.nth(i)).toHaveText(inputText)
+        }
     }
-    }
-    async chooseElementfromAutocompleteList(eqOfElement){
+    async chooseElementfromAutocompleteList(eqOfElement) {
         await this.autocompleteListElements.nth(eqOfElement).click()
     }
-    async submitFoodItem(){
+    async submitFoodItem() {
         await this.submitButton.click()
-        await expect (this.textField).toBeEmpty()
+        await expect(this.textField).toBeEmpty()
 
     }
 }
